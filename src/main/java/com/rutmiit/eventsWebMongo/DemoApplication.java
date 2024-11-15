62396ab9a84b87fe7a9e4e6f0219684de1996006
karalesus <karalesus@gmail.com>
2024-11-15 15:42:18 +0300

    add spring boot application

---
+++ src/main/java/com/rutmiit/eventsWebMongo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.rutmiit.eventsWebMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.rutmiit.eventsWebMongo")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
