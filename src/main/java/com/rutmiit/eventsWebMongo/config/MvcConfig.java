05ee4819238be09eeed14169b25759cc3060a69d
karalesus <karalesus@gmail.com>
2024-11-15 15:40:30 +0300

    add mvc config

---
+++ src/main/java/com/rutmiit/eventsWebMongo/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.rutmiit.eventsWebMongo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/").addResourceLocations("classpath:/static/index.html");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+}
